c3a76b19d131c7927ef515418cc0849907208c41
unknown <Claudionor@.(none)>
2011-08-13 00:47:53 -0300

    primeiro ccommit

---
+++ app/controllers/item_controller.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class ItemController extends AppController {
+    var $name = "Item";
+    var $scaffold;
+}
+?>
